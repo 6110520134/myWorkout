ae2c004d88d62b9ccf60647cc8ff92b0396e05d4
Natthaphong Chaowacharoenphong <6110520134@student.tu.ac.th>
2021-09-18 02:40:07 +0700

    Initial Commit

---
+++ MyWorkout/MyWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkoutApp.swift
+//  MyWorkout
+//
+//  Created by Nattapong Chaowhacharoenpong on 18/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
